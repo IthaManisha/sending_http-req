4288d5ff55b821dbc8acf1313c46c65313237197
IthaManisha <manisha.itha2001@gmail.com>
2023-08-15 12:19:45 +0530

    added get req & loading

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
+import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [isFetch,setIsFetch]=useState(false);
+
+  async function fetchMoviesHandler() {
+    setIsFetch(true);
+    const response=await fetch('https://swapi.dev/api/films/')
+      /*.then((response) => {
+        return response.json();
+      })*/
+      const data=await response.json();
+      //.then((data) => { setMovies(data.results);
+        const transformedMovies = data.results.map((movieData) => {
+          return {
+            id: movieData.episode_id,
+            title: movieData.title,
+            openingText: movieData.opening_crawl,
+            releaseDate: movieData.release_date,
+          };
+        });
+        
+        setMovies(transformedMovies);
+        setIsFetch(false);
+        
+      //});
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <section>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+      </section>
+      <section>
+        {!isFetch && movies.length>0 && <MoviesList movies={movies} />}
+        {!isFetch && movies.length===0 && <MoviesList movies={movies} />}
+        {isFetch && <p>Loading...</p>}
+      </section>
+    </React.Fragment>
   );
 }
 
