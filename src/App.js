093e0233e39f60cb5b092ee3f1c835880cd62b11
IthaManisha <manisha.itha2001@gmail.com>
2023-08-15 18:54:53 +0530

    http post request

--- src/App.js
+++ src/App.js
@@ -11,15 +11,24 @@ function App() {
 
   useEffect(()=>{fetchMoviesHandler();},[])
 
-  function addMovieHandler(movie) {
-    console.log(movie);
+  async function addMovieHandler(movie) {
+    const response=await fetch('https://react-http-e0629-default-rtdb.firebaseio.com/movies.json',{
+      method:'POST',
+      body:JSON.stringify(movie),
+      headers:{
+        'Content-Type':'application/jSON'
+      }
+    });
+    const data=await response.json();
+    console.log(data)
+
   }
 
   async function fetchMoviesHandler() {
     setIsFetch(true);
     setError(null);
     try{
-      const response=await fetch('https://swapi.dev/api/films/')
+      const response=await fetch('https://react-http-e0629-default-rtdb.firebaseio.com/movies.json')
       /*.then((response) => {
         return response.json();
       })*/
@@ -28,17 +37,30 @@ function App() {
         throw new Error('something went wrong!')
       }
       const data=await response.json();
+      console.log(data);
       //.then((data) => { setMovies(data.results);
-        const transformedMovies = data.results.map((movieData) => {
+      const loadedMovies=[];
+      for(const key in data)
+      {
+       loadedMovies.push(
+        {
+          id:key,
+          title:data[key].title,
+          openingText:data[key].openingText,
+          releaseDate:data[key].releaseDate
+        }
+       )
+      }
+        /*const transformedMovies = data.results.map((movieData) => {
           return {
             id: movieData.episode_id,
             title: movieData.title,
             openingText: movieData.opening_crawl,
             releaseDate: movieData.release_date,
           };
-        });
+        });*/
         
-        setMovies(transformedMovies);
+        setMovies(loadedMovies);
         //setIsFetch(false);
         
       //});
